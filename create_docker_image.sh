24ea9eddfe02848610d1a62cb14482987c9f5de5
Marco Massarelli <marco.massarelli@gmail.com>
2023-05-18 16:51:32 -0400

    Fix install script to refer to package.json

--- create_docker_image.sh
+++ create_docker_image.sh
@@ -4,7 +4,7 @@ sed -i .bak -E "s/version\"\: \"[^\"]+/version\": \"${ERGOGEN_VERSION}/g" packag
 sed -i .bak -E "s/github\:ergogen\/ergogen\#v[^\"]+/github:ergogen\/ergogen#v${ERGOGEN_VERSION}/g" package.json
 rm -rf node_modules
 rm -f package-lock.json
-npm install ergogen@${ERGOGEN_VERSION}
+npm install
 if [ -d node_modules ]; then
   docker build . -t ceoloide/docker-ergogen:${ERGOGEN_VERSION}
   docker push ceoloide/docker-ergogen:${ERGOGEN_VERSION}
