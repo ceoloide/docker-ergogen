9b23777f6b4b1dda665215fc2ba43ed59a4c69f3
Marco Massarelli <marco.massarelli@gmail.com>
2023-05-18 14:26:30 -0400

    Add additional explanation of the build.sh command

--- build.sh
+++ build.sh
@@ -5,4 +5,9 @@ container_cmd=docker
 # at the repo root (when running the script from there)
 container_args="-w /opt/ergogen -v $(pwd):/opt/ergogen --rm"
 
+# The following script mounts the current folder obtained with $(pwd) into /opt/ergogen in
+# the Docker image filesystem, and sets the working director to be the same. The --rm flag
+# ensures the image is deleted after running to clean up. Note that outputs are persisted
+# in the local filesystem since it got mounted into the Docker image, so they will remain
+# available after running.
 ${container_cmd} run ${container_args} ceoloide/docker-ergogen npm run ergogen input/ --clear
